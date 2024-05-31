58dc87b2e128da34c76d52a9f553dfd24b65d20a
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-05-31 09:22:17 +0200

    Node practise 4th module 1st part

--- src/routes/students.js
+++ src/routes/students.js
@@ -3,6 +3,9 @@
 import { Router } from 'express';
 import { getStudentsController, getStudentByIdController, createStudentController, deleteStudentController, upsertStudentController, patchStudentController } from '../controllers/students';
 import { ctrlWrapper } from '../utils/ctrlWrapper';
+import { validateBody } from '../middlewares/validateBody';
+import { updateStudentSchema } from '../validation/students';
+import { createStudentSchema } from '../validation/students';
 
 const router = Router();
 
@@ -10,9 +13,10 @@ const router = Router();
 
 router.get('/students', ctrlWrapper(getStudentsController));
 router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
-router.post('/students', ctrlWrapper(createStudentController));
+router.post('/students', validateBody(createStudentSchema), ctrlWrapper(createStudentController));
 router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
-router.put('/students/:studentId ', ctrlWrapper(upsertStudentController));
-router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+router.put('/students/:studentId ', validateBody(updateStudentSchema), ctrlWrapper(upsertStudentController));
+router.patch('/students/:studentId',validateBody(updateStudentSchema), ctrlWrapper(patchStudentController));
+
 
 export default router;
