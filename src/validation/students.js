7f621daec6a266a8409eed95959c6b513ed7f488
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-06-07 13:44:04 +0200

    last updation

--- src/validation/students.js
+++ src/validation/students.js
@@ -2,8 +2,13 @@
 
 import Joi from 'joi';
 
-export const createStudentSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+const createStudentSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required().messages({
+    'string.base': 'Username should be a string', // Кастомізація повідомлення для типу "string"
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
   email: Joi.string().email().required(),
   age: Joi.number().integer().min(6).max(16).required(),
   gender: Joi.string().valid('male', 'female', 'other').required(),
@@ -19,7 +24,9 @@ const dataToValidate = {
     avgMark: 10.2,
   };
 
-  const validationResult = createStudentSchema.validate(dataToValidate);
+  const validationResult = createStudentSchema.validate(dataToValidate, {
+    abortEarly:false,
+  });
   if (validationResult.error) {
     console.error(validationResult.error.message);
   } else {
